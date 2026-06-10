ba3aad31ad19badcec58defaa77d91e605ff4fcc
Nickname <nickname@example.com>
2026-06-10 09:59:55 +0900

    テスト

--- team1/src/main/java/scoremanager/LoginAction.java
+++ team1/src/main/java/scoremanager/LoginAction.java
@@ -5,11 +5,14 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import tool.Action;
  
+//ログイン画面を表示する
 public class LoginAction extends Action {
 	@Override
 	public void execute(
 		HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
+		
+		// ログイン画面へフォワード
 		request.getRequestDispatcher("/scoremanager/login.jsp")
 		.forward(request, response);
 	}
